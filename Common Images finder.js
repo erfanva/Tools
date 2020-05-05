4d8d4b4e2b02df9a16e5d13daba2ed20e2095320
erfanva <erfanv1@gmail.com>
2020-05-05 23:13:42 +0430

    Update Common Images finder.js

--- Common Images finder.js
+++ Common Images finder.js
@@ -4,7 +4,7 @@
 // @version      0.1
 // @description  try to take over the world!
 // @author       erfanva
-// @match        *
+// @match        *://*/*
 // @grant        none
 // ==/UserScript==
 
